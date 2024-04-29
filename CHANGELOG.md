072ce79c6d45af02fea7ce29365423a434dc78c2
Arthur <worldnomad@gmail.com>
2024-04-29 15:56:19 -0700

    v0.1.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.9 (2024-04-29)
+
+* Fix js in distro
+
 ## 0.1.8 (2024-04-16)
 
 * Refactorings for pypi deployment
--- netbox_napalm_plugin/__init__.py
+++ netbox_napalm_plugin/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Arthur Hanson"""
 __email__ = "ahanson@netboxlabs.com"
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 
 from extras.plugins import PluginConfig
